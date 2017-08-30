3ef7b7c7668e9021b3180efbd38c766de760baf7
avneet007 <avneetkumar007@gmail.com>
2017-08-30 20:51:01 +0530

    login list register complete

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.get('/', function (req, res, next) {
 });
 
 
-
 app.use(express.static('static'))
 
 
@@ -27,6 +26,89 @@ app.get("/userList", function (req, res, next) {
         res.json(obj);
     });
 })
+
+
+
+app.post("/login", function (req, res, next) {
+   
+
+      var file = './data.json'
+      jsonfile.readFile(file, function (err, result) {
+        var flag=false;
+        if (err) {
+            console.log("error in reading file");
+        } else {
+
+           console.log(result.length);
+          
+            for(var i=0;i<result.length;i++){
+
+                if(result[i].email==req.body.email && result[i].password==req.body.password){
+                    
+                     res.send(result[i].name);
+                     flag=true;
+                     break;
+                }
+            }
+
+            if(!flag){
+
+                res.send("no user found");
+            }
+
+
+        }
+
+    });
+
+
+})
+
+
+app.post("/saveUser", function (req, res, next) {
+
+  
+    var file = './data.json'
+    var obj = {
+        email: req.body.email,
+        password:req.body.password,
+        name: req.body.name
+
+    }
+
+    jsonfile.readFile(file, function (err, result) {
+        var flag=false;
+        if (err) {
+            console.log("error in reading file");
+        } else {
+
+           console.log(result.length);
+          
+            for(var i=0;i<result.length;i++){
+
+                if(result[i].email==req.body.email){
+                     res.send("email already registered");
+                     flag=true;
+                     break;
+                }
+            }
+         
+            if(!flag){
+            result.push(obj);
+            jsonfile.writeFile(file, result, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.send(obj);
+                }
+            })
+            }
+        }
+
+    });
+})
+
+
 
 
 
